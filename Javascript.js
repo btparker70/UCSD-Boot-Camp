7a179333e63a13bfa5159271113f8d3f44b4a6d2
btparker70 <33881192+btparker70@users.noreply.github.com>
2020-09-01 23:23:39 -0700

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,52 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.width = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = ".3";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.border = "thick solid #0000FF";
+
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0";
+
+});
+
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.borderRadius = "100px";
+});
+
+document.getElementById("button7").addEventListener("click", function(){
+
+    document.getElementById("box").style.boxShadow = "10px 20px 30px black";
+});
+
+document.getElementById("button8").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+    document.getElementById("box").style.border = "none";
+    document.getElementById("box").style.borderRadius = "0px";
+    document.getElementById("box").style.boxShadow = "0px 0px 0px white";
+
+    });
